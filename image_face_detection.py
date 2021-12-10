d47fee5014259db1af29cdd792a584ebb5cf4761
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-12-10 18:26:58 +0000

    Add an improved frontal face xml pre-trained model

--- image_face_detection.py
+++ image_face_detection.py
@@ -14,12 +14,12 @@ face_classifier = cv2.CascadeClassifier(classifier_file)
 # convert image to gray-scaled image for easier identification
 grayed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 face_coordinates = face_classifier.detectMultiScale(grayed_img)
-(x, y, w, h) = face_coordinates[0]
 
-#Drawing the rectangle on the image
-cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 5)
+for (x, y, w, h) in face_coordinates:
+    # Drawing the rectangle on the image or images if they are many
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 5)
 
-#showing the face detector app
+# showing the face detector app
 cv2.imshow('Danny Face Detector', img)
 
 cv2.waitKey()
